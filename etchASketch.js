d16e9f52119a2ed93f9f01a2bb70aefa8c3630eb
jturn241 <jturn241@gmail.com>
2021-08-22 17:03:53 -0400

    first hover

---
+++ etchASketch.js
@@ -0,0 +1,12 @@
+const containter = document.getElementById("containter");
+
+function makeRows(rows, cols) {
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+    }
+} 
+
+makeRows(16, 16);
