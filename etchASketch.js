92b4f3adc4e21a176e5e55b6e2498bc522645b0c
jturn241 <jturn241@gmail.com>
2021-08-23 15:03:15 -0400

    add hover freeze

--- etchASketch.js
+++ etchASketch.js
@@ -4,9 +4,17 @@ function makeRows(rows, cols) {
     container.style.setProperty("--grid-rows", rows);
     container.style.setProperty("--grid-cols", cols);
     for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement("div");
+        let cell = document.createElement("divs");
         container.appendChild(cell).className = "grid-item";
     }
 } 
 
-makeRows(16, 16);
+makeRows(20, 20);
+
+const squares = document.querySelectorAll("divs");
+
+squares.forEach(function(squares) {
+    squares.addEventListener("mouseenter", () => {
+        squares.classList.add('final');
+    });
+});
