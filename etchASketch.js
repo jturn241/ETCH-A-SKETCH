7cce3658a3ee060c078cdf394017e3ec92ecffad
jturn241 <jturn241@gmail.com>
2021-08-31 12:04:46 -0400

    added color change, fixed layout

--- etchASketch.js
+++ etchASketch.js
@@ -18,17 +18,58 @@ function gridDraw() {
     });
 };
 
-makeRows(16, 16);
+makeRows(30, 30);
 
 gridDraw();
 
 function reset () {
     let papa = document.getElementById("container")
     papa.querySelectorAll('*').forEach(n => n.remove());
-    let x = parseInt(prompt("Please enter grid size of 40 or less"));
-    while(!Number.isInteger(x) || x > 40) {
-        x = parseInt(prompt("Please enter a valid number"));
+    let x = parseInt(prompt("Please enter grid size of 100 or less"));
+    while(!Number.isInteger(x) || x > 100) {
+        x = parseInt(prompt("Please enter a valid number of 100 or less"));
     };
     makeRows(x, x);
     gridDraw(); 
+};
+
+var stopColor;
+
+function gridDrawRandom() {
+    stopColor = setTimeout(() =>{gridDrawRandom();}, 3000);
+    const squares = document.querySelectorAll("divs");
+    squares.forEach(function(squares) {
+        squares.addEventListener("mouseenter", function() {
+            this.style.backgroundColor = randomRGB();
+        });
+    });
+};
+
+function eraser() {
+    clearTimeout(stopColor);
+    const squares = document.querySelectorAll("divs");
+    squares.forEach(function(squares) {
+        squares.addEventListener("mouseenter", () => {
+            squares.classList.add('clear');
+        });
+    });
+};
+
+function randomRGB() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")"
+    return bgColor;
+}; 
+
+function randomColorReset() {
+    let papa = document.getElementById("container")
+    papa.querySelectorAll('*').forEach(n => n.remove());
+    let x = parseInt(prompt("Please enter grid size of 100 or less"));
+    while(!Number.isInteger(x) || x > 100) {
+        x = parseInt(prompt("Please enter a valid number of 100 or less"));
+    };
+    makeRows(x, x);
+    gridDrawRandom();
 };
